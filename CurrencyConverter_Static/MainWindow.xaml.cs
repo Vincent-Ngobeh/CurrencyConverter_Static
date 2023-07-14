66b916f09a08f55ac064440ff8ef2cca682e5c89
Vincent Sam Ngobeh <vincentngobeh@gmail.com>
2023-07-14 21:56:45 +0100

    creating a data table with all the currencies, implementing the convert button and user input authentication.

--- CurrencyConverter_Static/MainWindow.xaml.cs
+++ CurrencyConverter_Static/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Data;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -13,6 +14,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
+
 namespace CurrencyConverter_Static
 {
     /// <summary>
@@ -23,19 +26,83 @@ namespace CurrencyConverter_Static
         public MainWindow()
         {
             InitializeComponent();
-            lblCurrency.Content = "Hello World";
+            BindCurrency();
 
         }
 
+        private void BindCurrency()
+        {
+            DataTable dtCurrency = new DataTable();
+            dtCurrency.Columns.Add("Text");
+            dtCurrency.Columns.Add("Value");
+
+
+            //Add rows in Datatable with text and value
+            dtCurrency.Rows.Add("--SELECT--", 0);
+            dtCurrency.Rows.Add("INR", 1);
+            dtCurrency.Rows.Add("USD", 75);
+            dtCurrency.Rows.Add("EUR", 85);
+            dtCurrency.Rows.Add("SAR", 20);
+            dtCurrency.Rows.Add("POUND", 5);
+            dtCurrency.Rows.Add("DEM", 43);
+
+
+            cmbFromCurrency.ItemsSource = dtCurrency.DefaultView;
+            cmbFromCurrency.DisplayMemberPath = "Text";
+            cmbFromCurrency.DisplayMemberPath = "Value";
+            cmbFromCurrency.SelectedIndex = 0;
+
+            //All properties are set for 'To Currency' Combobox as 'From Currency' Combobox
+            cmbToCurrency.ItemsSource = dtCurrency.DefaultView;
+            cmbToCurrency.DisplayMemberPath = "Text";
+            cmbToCurrency.SelectedValuePath = "Value";
+            cmbToCurrency.SelectedIndex = 0;
+        }
+
+
         private void Convert_Click(object sender, RoutedEventArgs e)
         {
-            lblCurrency.Content = "Hello Button Clicker";
+            //Create a variable as ConvertedValue with double data type to store currency converted value
+            double ConvertedValue;
+
+            //Check amount textbox is Null or Blank
+            if (txtCurrency.Text == null || txtCurrency.Text.Trim() == "")
+            {
+                //If amount textbox is Null or Blank it will show the below message box   
+                MessageBox.Show("Please Enter Currency", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+
+                //After clicking on message box OK sets the Focus on amount textbox
+                txtCurrency.Focus();
+                return;
+            }
+            //Else if the currency from is not selected or it is default text --SELECT--
+            else if (cmbFromCurrency.SelectedValue == null || cmbFromCurrency.SelectedIndex == 0)
+            {
+                //It will show the message
+                MessageBox.Show("Please Select Currency From", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+
+                //Set focus on From Combobox
+                cmbFromCurrency.Focus();
+                return;
+            }
+            //Else if Currency To is not Selected or Select Default Text --SELECT--
+            else if (cmbToCurrency.SelectedValue == null || cmbToCurrency.SelectedIndex == 0)
+            {
+                //It will show the message
+                MessageBox.Show("Please Select Currency To", "Information", MessageBoxButton.OK, MessageBoxImage.Information);
+
+                //Set focus on To Combobox
+                cmbToCurrency.Focus();
+                return;
+            }
         }
 
         private void Clear_Click(object sender, RoutedEventArgs e)
         {
             lblCurrency.Content = "";
         }
+
+      
 
         private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
         {
