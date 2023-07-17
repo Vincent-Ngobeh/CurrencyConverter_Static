1913629bff3902b49d9ced8815542eab937ca564
Vincent Sam Ngobeh <vincentngobeh@gmail.com>
2023-07-18 00:44:32 +0100

    fixing bugs with the connection string and testing the buttons

--- CurrencyConverter_Static/obj/Debug/CurrencyConverter_Static_Content.g.i.cs
+++ CurrencyConverter_Static/obj/Debug/CurrencyConverter_Static_Content.g.i.cs
@@ -8,7 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("database/currencyconverter.mdf")]
 [assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("database/currencyconverter_log.ldf")]
 
 
